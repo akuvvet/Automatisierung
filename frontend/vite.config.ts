f071ab18b372a5b252a768c1438b65e101eeb3b4
Abdullah <you@example.com>
2025-12-17 11:04:25 +0100

    port 5175

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',
-    port: 5174,
+    host: '0.0.0.0',
+    port: 5175,
     strictPort: true,
     hmr: {
       protocol: 'ws',
       host: 'localhost',
-      port: 5174,
+      port: 5175,
     },
     proxy: {
       '/auth': {
